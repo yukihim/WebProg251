7a0b91b98832446808f03c2156c5d237eaf6ba19
Phạm Lê Quân <phamlequanwork@gmail.com>
2025-12-02 00:09:05 +0700

    Minor fixes

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -57,9 +57,5 @@
         </div>
     </div>
 </footer>
-
-
-<!-- Bootstrap JS Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -50,6 +50,9 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 
     <!-- Custom JS for effects functionality -->
     <script src="/quanswebsite/public/js/otherEffects.js"></script>
+
+    <!-- Bootstrap JS Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
